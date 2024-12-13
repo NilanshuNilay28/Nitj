1e96eece93926ee6d471989cefcc181bdc29915f
NilanshuNilay28 <100609377+NilanshuNilay28@users.noreply.github.com>
2024-12-13 21:47:09 +0530

    Add files via upload

---
+++ Semester-1/CPBP/Class_Work/calc.py
@@ -0,0 +1,56 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "59e4f079-1d76-450a-8915-930f014bb7f6",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# Import module\n",
+    "def add(a,b):\n",
+    "     return a+b"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "e9c730f9-8273-4884-870f-54ac112f168c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def sub(a,b):\n",
+    "    return a-b"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "d97ebfbf-af40-4914-95e1-97b8fd49b01c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
